e6d0d50614e353035374489584053dde056721a0
funnyh4wk <funnyh4wk@gmail.com>
2026-08-20 11:01:54 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -137,3 +137,8 @@ def delete_image(req: DeleteImageReq, user = Depends(verify_token)):
         return {"status": "ok"}
     except:
         return {"status": "error"}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
